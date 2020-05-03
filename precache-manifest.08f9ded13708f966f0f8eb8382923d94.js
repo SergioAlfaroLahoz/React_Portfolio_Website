d837c12063b7b6de93e7f53bcf221090481ff128
SergioAlfaroLahoz <SergioAlfaroLahoz@gmail.com>
2020-05-03 19:39:40 +0200

    Updates

---
+++ precache-manifest.08f9ded13708f966f0f8eb8382923d94.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11a01f930cddeaa9a82edafeb27d8a8",
+    "url": "/react-portfolio-website/index.html"
+  },
+  {
+    "revision": "61897eb72eabcf1cfa08",
+    "url": "/react-portfolio-website/static/css/main.d073b2a9.chunk.css"
+  },
+  {
+    "revision": "1dfad37691ce087665cd",
+    "url": "/react-portfolio-website/static/js/2.03c8e87c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio-website/static/js/2.03c8e87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61897eb72eabcf1cfa08",
+    "url": "/react-portfolio-website/static/js/main.ed2f8510.chunk.js"
+  },
+  {
+    "revision": "f1eef54379eb388c7fd2",
+    "url": "/react-portfolio-website/static/js/runtime-main.4d07d38f.js"
+  },
+  {
+    "revision": "c3ac4803e64542870d1a312d63030f15",
+    "url": "/react-portfolio-website/static/media/Automation_robot.c3ac4803.jpg"
+  },
+  {
+    "revision": "50160c6569d39df86042126737f44aa9",
+    "url": "/react-portfolio-website/static/media/Brain.50160c65.png"
+  },
+  {
+    "revision": "e0de9ffd3ab3b4e1be3161b78f400ece",
+    "url": "/react-portfolio-website/static/media/Electronic_board.e0de9ffd.jpg"
+  },
+  {
+    "revision": "7e641878ea458a890e4a178d66383078",
+    "url": "/react-portfolio-website/static/media/Galaxy.7e641878.jpg"
+  },
+  {
+    "revision": "6b22b2cf096a88aef78df0fa142fea26",
+    "url": "/react-portfolio-website/static/media/GitHub.6b22b2cf.png"
+  },
+  {
+    "revision": "5ae28da96e2f87270245cb80f25e372c",
+    "url": "/react-portfolio-website/static/media/Gmail.5ae28da9.png"
+  },
+  {
+    "revision": "32a9fda6dcef1c320c484c2d3c8750e4",
+    "url": "/react-portfolio-website/static/media/LinkedIn.32a9fda6.png"
+  },
+  {
+    "revision": "16c2ce8374bdcf10526e75efb222865b",
+    "url": "/react-portfolio-website/static/media/Sergio_Without_Background.16c2ce83.png"
+  },
+  {
+    "revision": "3951db304cc462117b045f9b537ad962",
+    "url": "/react-portfolio-website/static/media/Skills.3951db30.jpg"
+  },
+  {
+    "revision": "89bbb8fe9528ee94d284b030870a05bb",
+    "url": "/react-portfolio-website/static/media/Web_design.89bbb8fe.png"
+  }
+]);
