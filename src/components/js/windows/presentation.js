4a6d1d88ab8ac972781ef5c65d6d198b80816aff
Sergio Alfaro <sergioalfarolahoz@gmail.com>
2025-01-04 20:20:58 +0100

    breakline style adjustment

--- src/components/js/windows/presentation.js
+++ src/components/js/windows/presentation.js
@@ -12,7 +12,7 @@ export default class Presentation extends Component {
                 <h4>
                     I'm Sergio, an IoT, Electronics, and Automation Engineer 
                     passionate about creating smarter, connected solutions.
-                    <br/>I work as Product Owner at <a className="website" target="_blank" rel="noopener noreferrer" href="https://www.bsh-group.com/">BSH</a> - <a className="website" target="_blank" rel="noopener noreferrer" href="https://www.home-connect.com/global">Home Connect</a>.
+                    <br/>I work as Product Owner at <a className="website" target="_blank" rel="noopener noreferrer" href="https://www.bsh-group.com/">BSH</a> - <a className="website" target="_blank" rel="noopener noreferrer" href="https://www.home-connect.com/global">Home&nbsp;Connect</a>.
                 </h4>
             </div>    
         );
